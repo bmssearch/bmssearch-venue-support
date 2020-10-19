94d2332664be1bcd47711090f6e8b6909fc850ef
yuto-hasegawa <yuto-hasegawa@users.noreply.github.com>
2020-10-19 06:03:00 +0000

    deploy: bd2458fe5387cd803bb36e2c9bf49d3399d10516

---
+++ component---src-pages-404-tsx-1fd0b56258f2ff8bc215.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("9Dj+"),u=n("q1tI"),l=n.n(u),o=n("H8eV");t.default=function(){return l.a.createElement(a.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1fd0b56258f2ff8bc215.js.map
